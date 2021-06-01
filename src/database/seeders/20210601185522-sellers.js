d38dccb8a3d62b133a7a997df148c0768638ce60
Lucas dos Santos de Oliveira <lucax.oliveira96@gmail.com>
2021-06-01 18:31:13 -0300

    aaa

---
+++ src/database/seeders/20210601185522-sellers.js
@@ -0,0 +1,31 @@
+"use strict";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert(
+			"Sellers",
+			[
+				{
+					name: "José de Oliveira",
+					email: "j_oliveira@mail.com",
+					password: "123",
+				},
+				{
+					name: "Maria Carla",
+					email: "mcarla@mail.com",
+					password: "123",
+				},
+				{
+					name: "Felipe Candido",
+					email: "felipe@mail.com",
+					password: "123",
+				},
+			],
+			{}
+		);
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete("Sellers", null, {});
+	},
+};
